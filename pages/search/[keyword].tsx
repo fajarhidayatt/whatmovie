13500b8fc5d7b769119c2cb191a3cfcd064b3c8a
Fajar Hidayat <fajarhidayat0021@gmail.com>
2021-11-10 21:33:16 +0700

    add call API in SSR

--- pages/search/[keyword].tsx
+++ pages/search/[keyword].tsx
@@ -1,16 +1,19 @@
 import Footer from '../../components/organisms/Footer';
 import Navbar from '../../components/organisms/Navbar';
 import ResultMovies from '../../components/organisms/ResultMovies';
+import { getResultMovies } from '../../services/data_api';
+import { DetailMovieTypes } from '../../services/data_types';
 
 interface SearchProps {
   keyword: string;
+  movies: DetailMovieTypes[];
 }
 
-export default function Search({ keyword }: SearchProps) {
+export default function Search({ keyword, movies }: SearchProps) {
   return (
     <>
       <Navbar />
-      <ResultMovies keyword={keyword} />
+      <ResultMovies keyword={keyword} movies={movies} />
       <Footer />
     </>
   );
@@ -24,10 +27,13 @@ interface GetServerSideProps {
 
 export async function getServerSideProps({ params } : GetServerSideProps) {
   const { keyword } = params;
+  const response: any = await getResultMovies(keyword);
+  const movies = response.results;
 
   return {
     props: {
       keyword,
+      movies,
     },
   };
 }
