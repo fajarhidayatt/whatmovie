8a297ec9898dbd846cea19e1fe936541d33be9bd
Fajar Hidayat <fajarhidayat0021@gmail.com>
2021-11-10 21:30:29 +0700

    change class `dark-mode`

--- components/organisms/Navbar/ButtonTheme.tsx
+++ components/organisms/Navbar/ButtonTheme.tsx
@@ -9,9 +9,9 @@ export default function ButtonTheme() {
 
   useEffect(() => {
     if (isDark === 'true') {
-      document.body.classList.add('dark-mode');
+      document.documentElement.classList.add('dark-mode');
     } else {
-      document.body.classList.remove('dark-mode');
+      document.documentElement.classList.remove('dark-mode');
     }
   }, [isDark]);
 
