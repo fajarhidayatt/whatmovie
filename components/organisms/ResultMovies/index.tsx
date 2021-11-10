3b3fad8881e6b2621d048434cb6eb4f03d779a5e
Fajar Hidayat <fajarhidayat0021@gmail.com>
2021-11-10 21:31:32 +0700

    update call API in `search section`

--- components/organisms/ResultMovies/index.tsx
+++ components/organisms/ResultMovies/index.tsx
@@ -1,41 +1,20 @@
-import { useCallback, useEffect, useState } from 'react';
-import { getResultMovies } from '../../../services/data_api';
 import { DetailMovieTypes } from '../../../services/data_types';
 import MovieItem from '../../molecules/MovieItem';
 import MovieNotFound from './MovieNotFound';
 
 interface PopularMoviesProps {
   keyword: string;
+  movies: DetailMovieTypes[];
 }
 
-export default function ResultMovies({ keyword }: PopularMoviesProps) {
-  const [movies, setMovies] = useState([]);
-
-  const getResultMoviesAPI = useCallback(async (query) => {
-    if (query) {
-      const response: any = await getResultMovies(query);
-      setMovies(response.results);
-    }
-  }, []);
-
-  useEffect(() => {
-    getResultMoviesAPI(keyword);
-  }, [keyword]);
-
-  useEffect(() => {
-    setTimeout(() => {
-      const sectionResult = document.querySelector<HTMLElement>('.section-result')!;
-      sectionResult.style.display = 'block';
-    }, 500);
-  }, []);
-
+export default function ResultMovies({ keyword, movies }: PopularMoviesProps) {
   return (
     <div className="container-xxxl mt-5" style={{ minHeight: '100vh' }}>
       <div className="section-title mb-5 text-center">
         <h3 className="fw-bold mb-3">Search result for</h3>
         <h2 className="fw-bolder text-purple">{`"${keyword}"`}</h2>
       </div>
-      <div className="grid-wrapper flex-row flex-wrap section-result" style={{ display: 'none' }}>
+      <div className="grid-wrapper flex-row flex-wrap section-result">
         {(!keyword || movies.length === 0) ? (
           <div className="pt-3">
             <MovieNotFound />
