b306e879053f3cc04bc053463795c798358a66d4
Fajar Hidayat <fajarhidayat0021@gmail.com>
2021-11-10 21:25:37 +0700

    add info `crew`, `cast`, etc

--- components/organisms/DetailMovieContent/SectionInfo.tsx
+++ components/organisms/DetailMovieContent/SectionInfo.tsx
@@ -1,16 +1,31 @@
-import { DetailMovieTypes } from '../../../services/data_types';
+import CurrencyFormat from 'react-currency-format';
+import { CreditsTypes, CrewCastTypes, DetailMovieTypes } from '../../../services/data_types';
 
 interface SectionInfoProps {
     movie: DetailMovieTypes;
+    credits: CreditsTypes;
 }
 
 export default function SectionInfo(props: SectionInfoProps) {
-  const { movie } = props;
+  const { movie, credits } = props;
   const year = new Date(movie.release_date);
+  const rootImg = process.env.NEXT_PUBLIC_IMG;
+
+  const crew = credits.crew.filter((person: CrewCastTypes) => person.job === 'Director' || person.job === 'Story' || person.job === 'Creator' || person.job === 'Writer');
+  const cast = credits.cast.sort();
 
   return (
     <div className="section-info px-4 px-sm-5">
-      <div className="d-flex flex-column flex-sm-row gap-0 gap-sm-5 mb-0 mb-sm-3">
+      <div className="mb-4 mb-lg-0 info-crew">
+        {crew.map((person) => (
+          <div key={person.credit_id} className="crew">
+            <h5>{person.name}</h5>
+            <p>{person.job}</p>
+          </div>
+        ))}
+        <div />
+      </div>
+      <div className="info-movie mb-4">
         <div>
           <h5 className="fw-bold">Status</h5>
           <p>{movie.status}</p>
@@ -23,12 +38,44 @@ export default function SectionInfo(props: SectionInfoProps) {
           <h5 className="fw-bold">Duration</h5>
           <p>{`${movie.runtime} Min`}</p>
         </div>
+        <div>
+          <h5 className="fw-bold">Language</h5>
+          <p>{movie.original_language}</p>
+        </div>
+        <div>
+          <h5 className="fw-bold">Popularity</h5>
+          <p>{movie.popularity}</p>
+        </div>
+        <div>
+          <h5 className="fw-bold">Budget</h5>
+          <p><CurrencyFormat value={movie.budget} displayType="text" prefix="$" thousandSeparator /></p>
+        </div>
+        <div>
+          <h5 className="fw-bold">Revenue</h5>
+          <p><CurrencyFormat value={movie.revenue} displayType="text" prefix="$" thousandSeparator /></p>
+        </div>
       </div>
-      <div>
-        <h5 className="fw-bold overview">Overview</h5>
-        <p>{movie.overview}</p>
+      <div className="info-overview">
+        <div className="mb-4">
+          <h5 className="fw-bold overview">Overview</h5>
+          <p>{movie.overview}</p>
+        </div>
+        <div>
+          <h5 className="fw-bold mb-3">Cast</h5>
+          <div className="cast-wrapper">
+            {cast.map((person) => {
+              if (person.profile_path !== null) {
+                return (
+                  <div key={person.credit_id} className="cast-box">
+                    <img src={`${rootImg}/w92${person.profile_path}`} alt={person.name} className="img-cast" />
+                    <p>{person.original_name}</p>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </div>
       </div>
-
     </div>
   );
 }
